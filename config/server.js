a49ee2a874cdf5dbbe58e7688d2eb3469f777dc0
Adolfo Machado <adolfo.machado76@gmail.com>
2023-08-31 10:52:01 -0300

    Alterado Porta strapi para 1331

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1331),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'c8128d15a5fc1622aa0a4f948eb898b9'),
